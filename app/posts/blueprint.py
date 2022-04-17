807b1d5c22b088f3e33cf4978408fc39ad112ad7
Alexander <ReidSanya@yandex.ru>
2022-04-17 03:27:29 +0300

    Add search

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -3,12 +3,19 @@ from flask import render_template
 
 from models import Post, Tag
 
+from flask import request
+
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
 
