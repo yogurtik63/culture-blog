15c32c5e2e4fdb3abf62891fee4d483a95e33128
Alexander <ReidSanya@yandex.ru>
2022-04-17 05:28:24 +0300

    Pagination

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -2,21 +2,58 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post, Tag
+from .forms import PostForm
 
 from flask import request
+from app import db
+
+from flask import redirect
+from flask import url_for
 
 posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something wrong')
+        return redirect(url_for('posts.index'))
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
+@posts.route('/<slug>/edit/', methods=['POST', 'GET'])
+
+
 
 
 @posts.route('/')
 def index():
     q = request.args.get('q')
 
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
     else:
-        posts = Post.query.all()
-    return render_template('posts/index.html', posts=posts)
+        posts = Post.query.order_by(Post.created.desc())
+
+    pages = posts.paginate(page=page, per_page=5)
+
+    return render_template('posts/index.html', posts=posts, pages=pages)
 
 
 @posts.route('/<slug>')
