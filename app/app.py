b7ec78928c2bed614ec56496e46b13cc27699c6b
Alexander <ReidSanya@yandex.ru>
2022-04-17 06:13:47 +0300

    Add editing, admin panel

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -11,3 +13,9 @@ app.config.from_object(Configuration)
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+from models import *
+
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:parol999@localhost/test'
+    SECRET_KEY = 'my site its very mega secret!'
